ddc2d280b494f956d5a250bb0295fce381f9a923
Alejandro Padilla <apadilla@arrivelogistics.com>
2022-04-18 14:17:52 -0500

    Add project files.

---
+++ MonitorDatabaseEF/Controllers/DatabaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonitorDatabaseEF.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class DatabaseController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult PosteoMensaje()
+        {
+            return Ok();
+        }
+    }
+}
